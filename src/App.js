956f19f6ca6b6c4c71994678949822808c6ec53c
Steven Li <stevenli.581@gmail.com>
2021-07-13 18:54:25 -0400

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import Alert from 'react-bootstrap/Alert';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    [
+      'primary',
+      'secondary',
+      'success',
+      'danger',
+      'warning',
+      'info',
+      'light',
+      'dark',
+    ].map((variant, idx) => (
+      <Alert className="m-5" key={idx} variant={variant}>
+        This is a {variant} alert—check it out!
+      </Alert>
+    ))
   );
 }
 
